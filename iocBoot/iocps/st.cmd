ba9aa1bf962c8822259682c03005a6622c3b2a50
iocadm <iocadm@EpicsSummerSchool.myguest.virtualbox.org>
2025-08-21 13:43:34 +0200

    initial commit

---
+++ iocBoot/iocps/st.cmd
@@ -0,0 +1,22 @@
+#!../../bin/linux-x86_64/ps
+
+#- SPDX-FileCopyrightText: 2005 Argonne National Laboratory
+#-
+#- SPDX-License-Identifier: EPICS
+
+#- You may have to change ps to something else
+#- everywhere it appears in this file
+
+#< envPaths
+
+## Register all support components
+dbLoadDatabase "../../dbd/ps.dbd"
+ps_registerRecordDeviceDriver(pdbbase) 
+
+## Load record instances
+dbLoadRecords("../../psApp/Db/ps.db","user=iocadm")
+
+iocInit()
+
+## Start any sequence programs
+#seq sncps,"user=iocadm"
